7d1fa088eb879a9d98c620804af30775a2a51ccf
Awawa <mail4awawa@gmail.com>
2020-09-28 18:01:19 +0200

    Build system update

--- image_builder/stage-hyperbian/00-install-hyperion/00-run-chroot.sh
+++ image_builder/stage-hyperbian/00-install-hyperion/00-run-chroot.sh
@@ -3,8 +3,19 @@
 # Download latest release
 echo '           Download HyperHDR'
 cd /tmp
-wget https://github.com/awawa-dev/HyperHDR/releases/download/v8.2.0.8A/Hyperion-8.2.0.8A-Linux-armv7l.deb
-apt install ./Hyperion-8.2.0.8A-Linux-armv7l.deb
-rm Hyperion-8.2.0.8A-Linux-armv7l.deb
+
+if [ "$DEPLOY_DIR" != "${DEPLOY_DIR/v7/}" ]; then
+    echo 'ARM v7!!!!!!!!!!!!!!!'
+    wget https://github.com/awawa-dev/HyperHDR/releases/download/v8.2.0.8A2/HyperHDR-8.2.0.8A2-Linux-armv7l.deb
+    apt install ./HyperHDR-8.2.0.8A2-Linux-armv7l.deb
+    rm HyperHDR-8.2.0.8A2-Linux-armv7l.deb
+else
+    echo 'ARM v6!!!!!!!!!!!!!!!'
+    wget https://github.com/awawa-dev/HyperHDR/releases/download/v8.2.0.8A2/HyperHDR-8.2.0.8A2-Linux-armv6l.deb
+    apt install ./HyperHDR-8.2.0.8A2-Linux-armv6l.deb
+    rm HyperHDR-8.2.0.8A2-Linux-armv6l.deb
+fi
+
+
 
 
