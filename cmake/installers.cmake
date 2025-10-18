fe00209ca46d17d774ee0b5c841f0df753beb441
Awawa <69086569+awawa-dev@users.noreply.github.com>
2025-10-18 21:06:05 +0200

    Update installers.cmake

--- cmake/installers.cmake
+++ cmake/installers.cmake
@@ -3,11 +3,7 @@ macro(DeployApple TARGET)
 		cmake_policy(PUSH)
 		cmake_policy(SET CMP0177 NEW)	
 		install ( TARGETS hyperhdr DESTINATION "share/.." COMPONENT "HyperHDR" )
-		cmake_policy(POP)
-
-		set(HYPERHDR_BUNDLE_DIR "${CMAKE_INSTALL_PREFIX}/hyperhdr.app")
-		message("Bundle destination: ${HYPERHDR_BUNDLE_DIR}")
-		install(CODE "set(HYPERHDR_BUNDLE_DIR \"${HYPERHDR_BUNDLE_DIR}\")" COMPONENT "HyperHDR")
+		cmake_policy(POP)		
 
 		install(FILES "${PROJECT_SOURCE_DIR}/cmake/osx/Hyperhdr.icns" DESTINATION "hyperhdr.app/Contents/Resources" COMPONENT "HyperHDR")
 		install(FILES "${PROJECT_SOURCE_DIR}/LICENSE" DESTINATION "hyperhdr.app/Contents/Resources" COMPONENT "HyperHDR")
@@ -15,23 +11,23 @@ macro(DeployApple TARGET)
 
 		# Copy QMQTT
 		if (USE_SHARED_LIBS AND TARGET qmqtt)
-			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:qmqtt> DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
-			install(CODE [[ file(INSTALL FILES $<TARGET_SONAME_FILE:qmqtt> DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
+			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:qmqtt> DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
+			install(CODE [[ file(INSTALL FILES $<TARGET_SONAME_FILE:qmqtt> DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
 		endif()
 
 		# Copy SQLITE3
 		if (USE_SHARED_LIBS AND TARGET sqlite3)
-			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:sqlite3> DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
+			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:sqlite3> DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
 		endif()
 
 		# Copy utils-zstd
 		if (USE_SHARED_LIBS AND TARGET utils-zstd)
-			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:utils-zstd> DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
+			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:utils-zstd> DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
 		endif()
 
 		# Copy utils-image
 		if (USE_SHARED_LIBS AND TARGET utils-image)
-			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:utils-image> DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
+			install(CODE [[ file(INSTALL FILES $<TARGET_FILE:utils-image> DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib" TYPE SHARED_LIBRARY) ]] COMPONENT "HyperHDR")
 		endif()
 
 		if ( Qt5Core_FOUND )			
@@ -45,7 +41,7 @@ macro(DeployApple TARGET)
 			OUTPUT_VARIABLE MYQT_PLUGINS_DIR
 			OUTPUT_STRIP_TRAILING_WHITESPACE
 		)
-		
+
 		install(CODE "set(MYQT_PLUGINS_DIR \"${MYQT_PLUGINS_DIR}\")"     COMPONENT "HyperHDR")
 		install(CODE "set(MY_DEPENDENCY_PATHS \"${TARGET_FILE}\")"       COMPONENT "HyperHDR")
 		install(CODE "set(MY_SYSTEM_LIBS_SKIP \"${SYSTEM_LIBS_SKIP}\")"  COMPONENT "HyperHDR")
@@ -75,12 +71,16 @@ macro(DeployApple TARGET)
 					foreach(openssl_lib ${filesSSL})
 						string(FIND ${openssl_lib} "dylib" _indexSSL)
 						if (${_indexSSL} GREATER -1)
-							file(COPY "${openssl_lib}"
-								DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/Frameworks"
+							file(INSTALL
+								DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/Frameworks"
+								TYPE SHARED_LIBRARY
+								FILES "${openssl_lib}"
 							)
 						else()
-							file(COPY "${openssl_lib}"
-								DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib"
+							file(INSTALL
+								DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib"
+								TYPE SHARED_LIBRARY
+								FILES "${openssl_lib}"
 							)
 						endif()
 					endforeach()
@@ -93,19 +93,20 @@ macro(DeployApple TARGET)
 					RESOLVED_DEPENDENCIES_VAR _r_deps
 					UNRESOLVED_DEPENDENCIES_VAR _u_deps
 				)
-
-				file(MAKE_DIRECTORY "${HYPERHDR_BUNDLE_DIR}/Contents/Frameworks")
-				file(MAKE_DIRECTORY "${HYPERHDR_BUNDLE_DIR}/Contents/lib")
-
+				  
 				foreach(_file ${_r_deps})										
 					string(FIND ${_file} "dylib" _index)
 					if (${_index} GREATER -1)
-						file(COPY "${_file}"
-							DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/Frameworks"
+						file(INSTALL
+							DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/Frameworks"
+							TYPE SHARED_LIBRARY
+							FILES "${_file}"
 						)
 					else()
-						file(COPY "${_file}"
-							DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib"
+						file(INSTALL
+							DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib"
+							TYPE SHARED_LIBRARY
+							FILES "${_file}"
 						)
 					endif()
 				endforeach()				
@@ -126,15 +127,16 @@ macro(DeployApple TARGET)
 								)
 
 							foreach(DEPENDENCY ${PLUGINS})
-									file(COPY "${DEPENDENCY}"
-										DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/lib"
+									file(INSTALL
+										DESTINATION "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib"
+										TYPE SHARED_LIBRARY
+										FILES ${DEPENDENCY}
 									)									
 							endforeach()
 
 							get_filename_component(real_file "${file}" REALPATH)
 							get_filename_component(singleQtLib ${file} NAME)
 							list(APPEND MYQT_PLUGINS "${HYPERHDR_BUNDLE_DIR}/Contents/plugins/${PLUGIN}/${singleQtLib}")
-							message("Copying real plugin ${real_file} to ${HYPERHDR_BUNDLE_DIR}/Contents/plugins/${PLUGIN}")
 							file(MAKE_DIRECTORY "${HYPERHDR_BUNDLE_DIR}/Contents/plugins/${PLUGIN}")
 							file(COPY "${real_file}"
 								DESTINATION "${HYPERHDR_BUNDLE_DIR}/Contents/plugins/${PLUGIN}"
@@ -145,10 +147,10 @@ macro(DeployApple TARGET)
 				endforeach()
 
 			include(BundleUtilities)										
-			fixup_bundle("${HYPERHDR_BUNDLE_DIR}" "${MYQT_PLUGINS}" "${HYPERHDR_BUNDLE_DIR}/Contents/lib")
-			fixup_bundle("${HYPERHDR_BUNDLE_DIR}" "" "")
+			fixup_bundle("${CMAKE_INSTALL_PREFIX}/hyperhdr.app" "${MYQT_PLUGINS}" "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib")
+			fixup_bundle("${CMAKE_INSTALL_PREFIX}/hyperhdr.app" "" "")
 				
-			file(REMOVE_RECURSE "${HYPERHDR_BUNDLE_DIR}/Contents/lib")
+			file(REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/lib")			
 			file(REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}/share")
 
 			message( "Detected architecture: '${SCOPE_CMAKE_SYSTEM_PROCESSOR}'")
@@ -156,8 +158,8 @@ macro(DeployApple TARGET)
 				cmake_policy(PUSH)
 					cmake_policy(SET CMP0009 NEW)
 					message( "Re-signing bundle's components...")
-					file(GLOB_RECURSE libSignFramework LIST_DIRECTORIES false "${HYPERHDR_BUNDLE_DIR}/Contents/Frameworks/*")
-					list(APPEND libSignFramework "${HYPERHDR_BUNDLE_DIR}/Contents/MacOS/hyperhdr")
+					file(GLOB_RECURSE libSignFramework LIST_DIRECTORIES false "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/Frameworks/*")
+					list(APPEND libSignFramework "${CMAKE_INSTALL_PREFIX}/hyperhdr.app/Contents/MacOS/hyperhdr")
 					foreach(_fileToSign ${libSignFramework})
 						string(FIND ${_fileToSign} ".framework/Resources" isResources)
 						if (${isResources} EQUAL -1)
@@ -168,7 +170,7 @@ macro(DeployApple TARGET)
 						endif()			
 					endforeach()
 					message( "Perform final verification...")
-					execute_process(COMMAND bash -c "codesign --verify --deep -vvvv ${HYPERHDR_BUNDLE_DIR}" RESULT_VARIABLE CODESIGN_VERIFY)
+					execute_process(COMMAND bash -c "codesign --verify --deep -vvvv ${CMAKE_INSTALL_PREFIX}/hyperhdr.app" RESULT_VARIABLE CODESIGN_VERIFY)
 					if(NOT CODESIGN_VERIFY EQUAL 0)
 						message(WARNING "Failed to repair the bundle signature: verification failed")
 					endif()
